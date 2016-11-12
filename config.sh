f0503cd2a4e018bb445a30fe5e8bc8924a47c26d
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2016-11-12 18:08:15 +0100

    Add freeboxplayer_url variable

--- config.sh
+++ config.sh
@@ -1,2 +1,3 @@
 freeboxplayer_remotecode="12345678"
 freeboxplayer_hdn="hd1"
+freeboxplayer_url="http://$freeboxplayer_hdn.freebox.fr/pub/remote_control?code=$freeboxplayer_remotecode&key="# DO NOT CHANGE!
