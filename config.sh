add24165ed51518d4791ec5012b1f6a48d832a6a
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2016-11-15 22:38:21 +0100

    Delete $freeboxplayer_url variable

--- config.sh
+++ config.sh
@@ -1,3 +1,2 @@
 freeboxplayer_remotecode="12345678"
 freeboxplayer_hdn="hd1"
-freeboxplayer_url="http://$freeboxplayer_hdn.freebox.fr/pub/remote_control?code=$freeboxplayer_remotecode&key="# DO NOT CHANGE!
